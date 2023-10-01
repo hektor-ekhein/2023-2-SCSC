c8334624fbf3180db01ce3f7ecfea7597fbee68c
hektor-ekhein <johngalt827@snu.ac.kr>
2023-10-01 20:53:25 +0900

    Added 2023.10.01 algorithm seminar stuff

--- algorithm_seminar/2023.10.01_algorithms.cpp
+++ algorithm_seminar/2023.10.01_algorithms.cpp
@@ -2,6 +2,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 /*
+
+// 수 정렬하기 3
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -23,7 +25,7 @@ int main(){
 }
 */
 /*
-
+// 단어 정렬
 bool compareByLength(const string &a, const string &b){
     if(a.length() == b.length()){
         return a < b;
@@ -53,6 +55,9 @@ int main(){
 
 */
 /*
+
+//소트인사이드
+
 int main(){
     string s;
     cin >> s;
@@ -69,6 +74,9 @@ int main(){
 }
 */
 /*
+
+// 수 정렬하기 2
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -87,6 +95,9 @@ int main(){
 }
 */
 /*
+
+//나이순 정렬
+
 bool compareByAge(const pair<pair<int, string>,int> &a, const pair<pair<int, string>,int> &b){
     if (a.first.first == b.first.first){
         return a.second<b.second;
@@ -113,6 +124,9 @@ int main(){
 }
 */
 /*
+
+//좌표 정렬하기 2
+
 bool compareByCoords(const pair<int, int> &a, const pair<int, int> &b){
     if(a.first == b.first){
         return a.second < b.second;
@@ -138,6 +152,9 @@ int main(){
 }
 */
 /*
+
+//solved.ac
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -164,6 +181,9 @@ int main(){
 }
 */
 /*
+
+// Easy Work
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -190,6 +210,7 @@ int main(){
 }
 */
 /*
+// 대칭 차집합
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -220,6 +241,165 @@ int main(){
         }
     }
     cout << v.size() << "\n";
+    return 0;
+}
+*/
+
+/*
+// best 대칭 차집합
+set <int> A, B;
+while (N--){
+    int x; cin>> x; A.insert(x);
+}
+while (M--){
+    int x; cin>> x; B.insert(x);
+}
+set<int> difference;
+set_difference(A.begin(), A.end(), B.begin(), B.end(), inserter(difference, difference.begin()));
+cout << A.size() + B.size() - 2 * difference.size() << '\n';
+*/
+
+/*
+// 나는야 포켓몬 마스터 이다솜
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N,M;
+    cin >> N >> M;
+    unordered_map<string, int> m;
+    unordered_map<int, string> rm;
+    for(int i=0; i<N; ++i){
+        string in;
+        cin >> in;
+        m[in]=i+1;
+        rm[i+1]=in;
+    }
+    for(int i=0; i<M; ++i){
+        string in;
+        cin >> in;
+        if(isdigit(in[0])){
+            cout << rm[stoi(in)] << "\n";
+        }
+        else{
+            cout << m[in] << "\n";
+        }
+    }
+    return 0;
+}
+*/
+
+/*
+
+// 차집합
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N,M;
+    cin >> N >> M;
+    set <int> A, B;
+    while (N--){
+        int x; cin>> x; A.insert(x);
+    }
+    while (M--){
+        int x; cin>> x; B.insert(x);
+    }
+    set<int> difference;
+    set_difference(A.begin(), A.end(), B.begin(), B.end(), inserter(difference, difference.begin()));
+    cout << difference.size() << '\n';
+    if (difference.size()==0) return 0;
+    for (auto it=difference.begin();it!=difference.end();++it){
+        cout << *it << " ";
+    }
+    return 0;
+}
+
+*/
+/*
+// 최소 힙
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin >> N;
+    priority_queue<int, vector<int>, greater<int>> pq_min;
+    while(N--){
+        int in;
+        cin >> in;
+        if(in == 0){
+            if(pq_min.empty()){
+                cout << 0 << "\n";
+            }
+            else{
+                cout << pq_min.top() << "\n";
+                pq_min.pop();
+            }
+        }
+        else{
+            pq_min.push(in);
+        }
+    }
+    return 0;
+}
+*/
+
+
+/*
+// 집합의 표현
+
+class UnionFind{
+    private:
+        vector<int> p, rank, setSize;
+    public:
+        UnionFind(int N){
+            p.assign(N,0);
+            for (int i=0;i<N;++i) p[i]=i;
+            rank.assign(N,0);
+            setSize.assign(N,1);
+        }
+        int findSet(int i){
+            return (p[i]==i) ? i : (p[i]=findSet(p[i]));
+        }
+        bool isSameSet(int i, int j){
+            return findSet(i) == findSet(j);
+        }
+        int sizeOfSet(int i){
+            return setSize[findSet(i)];
+        }
+        void unionSet(int i,int j){
+            if(isSameSet(i,j)) return;
+            int x=findSet(i), y=findSet(j);
+            if(rank[x]>rank[y]) swap(x,y);
+            p[x]=y;
+            if (rank[x]==rank[y]) ++rank[y];
+            setSize[y]+=setSize[x];
+        }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n,m; cin >> n >> m;
+    UnionFind uf(n+1);
+    while(m--){
+        int op, a, b;
+        cin >> op >> a >> b;
+        if(op==0){
+            uf.unionSet(a,b);
+        }
+        else{
+            if(uf.isSameSet(a,b)){
+                cout << "YES" << "\n";
+            }
+            else{
+                cout << "NO" << "\n";
+            }
+        }
+    }
     return 0;
 }
 */
