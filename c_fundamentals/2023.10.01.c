3dad204c248ccb77376127de8fd25e1deb1911b7
hektor-ekhein <johngalt827@snu.ac.kr>
2023-10-07 14:02:08 +0900

    Added tidbits from Ch.0~Ch.2 of Modoocode C

---
+++ c_fundamentals/2023.10.01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+int main(){
+    printf("Hello World!\n");
+    return 0;
+}
+*/
+
+int main(){
+    int a;
+    a=10;
+    printf("a's value is: %d \n", a);
+    return 0;
+}
